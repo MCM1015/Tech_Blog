b2e66d3a3c0142ffdbf86e5378ae002393bfdae4
Mary McClernan <mary.c.mcclernan@gmail.com>
2021-06-08 20:23:12 -0400

    base files created, all routes working

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,65 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+//get route
+router.get('/', async (req, res) => {
+    try {
+      const commentData= await Comment.findAll();
+  
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+//Comment route
+router.post('/', async (req, res) => {
+    try {
+      const newComment = await Comment.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newComment);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+//put route
+router.put('/:id', async (req, res) => {
+    try {
+      const updateComment = await Comment.update(req.body, {
+        where: {
+            id: req.params.id
+            //user_id: req.session.user_id,
+        }
+      });
+      res.status(200).json(updateComment);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+//delete route
+router.delete('/:id', async (req, res) => {
+    try {
+      const commentData = await Comment.destroy({
+        where: {
+          id: req.params.id,
+          //user_id: req.session.user_id,
+        },
+      });
+  
+      if (!commentData) {
+        res.status(404).json({ message: 'No Comment found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const { User, Comment, Post } = require('../models');
+const withAuth = require('../utils/auth')
+
+// Get Login
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/homepage');
+        return;
+    }
+
+    res.render('login');
+})
+
+// Get Homepage
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [{ model: Comment }]
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+        console.log(posts);
+
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Get Dashboard
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [{ model: Comment }],
+            where: {
+                user_id: req.session.user_id
+            }
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+        console.log(posts);
+
+        res.render('dashboard', {
+            posts,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+module.exports = router;
